b071598211c408ecefaf00518e9719d365c76f31
Seidy <seidyalanis@gmail.com>
2025-11-22 21:14:03 -0600

    eliminación del route destroy, porque no se va a eliminar perfiles

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 // Ruta para activar la cuenta mediante token (publica)

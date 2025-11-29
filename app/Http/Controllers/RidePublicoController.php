64b5598efb026becaa02ebd84727ff19d84b9ed5
Seidy <seidyalanis@gmail.com>
2025-11-28 22:52:18 -0600

    Inclusión de función index en RidePuclicController para hacer el filtro en salida, llegada y fecha.

--- app/Http/Controllers/RidePublicoController.php
+++ app/Http/Controllers/RidePublicoController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ride;
 use Illuminate\Http\Request;
 
-class RidePublicoController extends Controller
+class RidePublicController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = Ride::query();
+
+        // Filtros
+        if ($request->salida) {
+            $query->where('salida','LIKE',"%".$request->salida."%");
+        }
+
+        if ($request->llegada) {
+            $query->where('llegada','LIKE',"%".$request->llegada."%");
+        }
+
+        // Ordenamiento
+        if ($request->orden == 'fecha') {
+            $query->orderBy('dia');
+        } elseif ($request->orden == 'origen') {
+            $query->orderBy('salida');
+        } elseif ($request->orden == 'destino') {
+            $query->orderBy('llegada');
+        }
+
+        $rides = $query->get();
+
+        return view('publico.rides', compact('rides'));
+    }
 }

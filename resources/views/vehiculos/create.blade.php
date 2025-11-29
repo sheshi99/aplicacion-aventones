49c6b36be5725143944c64e9988f7724fd98c6fb
walbyn <walbyngonzalez@gmail.com>
2025-11-28 23:54:57 -0600

    Se ajusto el estilo de los botones aceptar y cancelar del formulario de agregar vehiculo

--- resources/views/vehiculos/create.blade.php
+++ resources/views/vehiculos/create.blade.php
@@ -54,9 +54,10 @@
                 </div>
             </div>
 
-            <button class="btn btn-success">Guardar Vehículo</button>
-            <a href="{{ route('vehiculos.index') }}" class="btn btn-secondary">Cancelar</a>
-
+            <div class="d-flex justify-content-end gap-2 mt-30">
+                <button type="submit" class="btn btn-success">Guardar Vehículo</button>
+                <a href="{{ route('vehiculos.index') }}" class="btn btn-danger">Cancelar</a>
+            </div>
         </div>
     </form>
 
